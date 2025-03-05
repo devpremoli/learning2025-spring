2a4a444afd275b5d58273a2ba5318bda569beae9
Prem Oli <devpremoli@gmail.com>
2025-03-04 20:26:16 -0600

    spring webflux

---
+++ spring-webflux/hello-world/src/main/java/com/devpremoli/helloworld/HelloWorldApplication.java
@@ -0,0 +1,16 @@
+package com.devpremoli.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(HelloWorldApplication.class, args);
+		GreetingClient greetingClient = context.getBean(GreetingClient.class);
+		// We need to block for the content here or the JVM might exit before the message is logged
+		System.out.println(">> message = " + greetingClient.getMessage().block());
+	}
+}
